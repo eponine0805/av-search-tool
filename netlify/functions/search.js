4cf72e091c5bb91bbc734158140ba8931d142f15
eponine0805 <umaumasei1rou@gmail.com>
2025-09-22 02:05:33 +0900

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -172,7 +172,7 @@ async function searchSokmil(keyword) {
     };
 
     const keywordPromises = keywords.map(kw => fetchSokmilApi(new URLSearchParams({ ...baseParams, keyword: kw })));
-    const actorPromises = actors.map(kw => fetchSokmilApi(new URLSearchParams({ ...baseParams, article: 'actress' })));
+    const actorPromises = actors.map(kw => fetchSokmilApi(new URLSearchParams({ ...baseParams, keyword: kw, article: 'actor' })));
 
     const allPromises = [...actorPromises, ...keywordPromises];
     const allResults = await Promise.all(allPromises);
@@ -299,11 +299,7 @@ async function searchDmm(keyword) {
 
     // 1. 「キーワード」と「女優」でAPIリクエストを分けて作成
     const keywordPromises = keywords.map(kw => fetchDmmApi(new URLSearchParams({ ...baseParams, keyword: kw })));
-    const actorPromises = actors.map(kw => {
-        const params = new URLSearchParams(baseParams);
-        params.append('article[0]', 'actress'); // パラメータ名を配列形式に変更
-        return fetchDmmApi(params);
-    });
+    const actorPromises = actors.map(kw => fetchDmmApi(new URLSearchParams({ ...baseParams, article, actress: 'actor' })));
 
     // 2. すべての検索を並列実行し、結果を一つにまとめる (元のシンプルなロジックに戻す)
     const allPromises = [...actorPromises, ...keywordPromises];
